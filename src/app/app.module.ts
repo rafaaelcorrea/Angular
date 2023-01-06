d58e541ca4bc828a644de414b4209770f9044417
Rafael Correa <lumia.1986@outlook.com>
2023-01-06 09:11:33 -0300

    Aula inicial/conceitual e criando os primeiros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';// Importamos esse componente da pasta raiz dele
+import { ServersComponent } from './servers/servers.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,//Declaramos esse componente que criamos. 
+    ServersComponent 
   ],
   imports: [
     BrowserModule
